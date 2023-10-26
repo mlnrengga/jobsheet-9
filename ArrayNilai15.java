d424baaee8407c06c62e093bca8b4e8bab53f311
mlnrengga <mlnrengga@gmail.com>
2023-10-26 22:06:56 +0700

    ;Modify

--- ArrayNilai15.java
+++ ArrayNilai15.java
@@ -5,12 +5,17 @@ public class ArrayNilai15 {
         Scanner input15 = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan Nilai Akhir Ke-"+i+" : ");
             nilaiAkhir[i] = input15.nextInt();
         }
         for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai Akhir Ke-"+i+" Adalah "+nilaiAkhir[i]);
+            if (nilaiAkhir[i] > 70){
+            System.out.println("Mahasiswa Ke-"+i+" Lulus! ");
+            }else if (nilaiAkhir[i] <= 70){
+            System.out.println("Mahasiswa Ke-"+i+" Tidak Lulus! ");    
+            }
+
         } 
         input15.close();
     }
