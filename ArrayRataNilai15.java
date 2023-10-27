403dcbab6f006fafb3653382c973cbbe77ed59b3
mlnrengga <mlnrengga@gmail.com>
2023-10-27 09:58:57 +0700

    ;Modify

--- ArrayRataNilai15.java
+++ ArrayRataNilai15.java
@@ -3,19 +3,26 @@ import java.util.Scanner;
 public class ArrayRataNilai15 {
     public static void main(String[] args) {
         Scanner input15 = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukkan Jumlah Mahasiswa : ");
+        int jmlMhs = input15.nextInt();
+        int[] nilaiMhs = new int[jmlMhs];
+        double rata2Lulus = 0, rata2Tidak = 0;
+        int jmlLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++){
             System.out.print("Masukkan Nilai Mahasiswa Ke-"+(i+1)+" : ");
             nilaiMhs[i] = input15.nextInt();
+            if (nilaiMhs[i] > 70) {
+                rata2Lulus += nilaiMhs[i];
+                jmlLulus += 1;
+            }else 
+                rata2Tidak += nilaiMhs[i];
         }
-        for (int i = 0; i < nilaiMhs.length; i++){
-            total += nilaiMhs[i];
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-Rata Nilai = "+rata2);
+        rata2Lulus = rata2Lulus/jmlLulus;
+        rata2Tidak = rata2Tidak/(nilaiMhs.length-jmlLulus);
+        System.out.println("Rata-Rata Nilai Lulus = "+rata2Lulus);
+        System.out.println("Rata-Rata Nilai Tidak Lulus = "+rata2Tidak);
+
         input15.close();
     }
     
